d99037715b4c66c553b9c3efeb00ea457e5d9a9b
RR-2000 <f20190115@goa.bits-pilani.ac.in>
2024-07-29 01:44:06 +0200

    add batch_train.sh

---
+++ batch_train.sh
@@ -0,0 +1,16 @@
+# python src/train.py --config=configs/bce_loss.yaml
+# python src/train.py --config=configs/focal_loss.yaml
+# python src/train.py --config=configs/dice_loss.yaml
+# python src/train.py --config=configs/bce-timm-101_26w_4s.yaml
+# python src/train.py --config=configs/bce-timm-enb6.yaml
+# python src/train.py --config=configs/bce-timm50d.yaml
+# python src/train.py --config=configs/bce-timm101e.yaml
+# python src/train.py --config=configs/patch-bce-timm-101_26w_4s.yaml
+# python src/train.py --config=configs/patch-bce-timm-enb6.yaml
+# python src/train.py --config=configs/patch-bce-timm50d.yaml
+# python src/train.py --config=configs/patch-bce-timm101e.yaml
+
+python src/train.py --config=configs/bce-enetb6.yaml
+python src/train.py --config=configs/bce-inceptionv4.yaml
+python src/train.py --config=configs/bce-resnet101.yaml
+python src/train.py --config=configs/bce-xception.yaml
