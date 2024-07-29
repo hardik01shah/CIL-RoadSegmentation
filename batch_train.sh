5a180b084b3a7c027ea3081f2f4dc6b93f7d51eb
RR-2000 <f20190115@goa.bits-pilani.ac.in>
2024-07-29 12:17:49 +0200

    mobileNet Add

--- batch_train.sh
+++ batch_train.sh
@@ -10,7 +10,7 @@
 # python src/train.py --config=configs/patch-bce-timm50d.yaml
 # python src/train.py --config=configs/patch-bce-timm101e.yaml
 
-python src/train.py --config=configs/bce-enetb6.yaml
+# python src/train.py --config=configs/bce-enetb6.yaml
 python src/train.py --config=configs/bce-inceptionv4.yaml
-python src/train.py --config=configs/bce-resnet101.yaml
-python src/train.py --config=configs/bce-xception.yaml
+# python src/train.py --config=configs/bce-resnet101.yaml
+python src/train.py --config=configs/bce-mobilenet_v2.yaml
